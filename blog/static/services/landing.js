ca7bc7f8ca823ab0131ac0a640edcc430f80e28e
silindokuhle@invantage.co.za <silindokuhle@invantage.co.za>
2024-09-01 03:48:53 +0200

    Comment out the logout method in the landing file as I'm no longer using it for a logout

--- blog/static/services/landing.js
+++ blog/static/services/landing.js
@@ -1,15 +1,10 @@
-async function logout() {
-
-    alert(window.location.host);
-
-        try {
-            const response = await fetch("/login/");
-            if (!response.ok) {
-                throw new Error("Network response was not OK");
-            }
-            const todos = await response.json();
-            console.log(todos);
-        } catch (error) {
-            console.error("There was a problem with your fetch request: ", error);
-        }
-}
+//async function logout() {
+//    try {
+//        const response = await red("/login/");
+//        if (!response.ok) {
+//            throw new Error("Network response was not OK");
+//        }
+//    } catch (error) {
+//        console.error("There was a problem with your fetch request: ", error);
+//    }
+//}
