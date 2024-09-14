89c6ce37848b9545efe6aa82d53d795df2c4569f
silindokuhle@invantage.co.za <silindokuhle@invantage.co.za>
2024-09-14 05:14:05 +0200

    Add a change to enable the edit comment and delete comment screens to go back to the add comments view screen,
    The system has been submitted to TUT on the 13th for the SLP programme.
    
    Enhancements, testing and code refactoring are still ongoing.
    
    Features for the Software Innnovative Ideas blog:
    * Add a blog post
    * Edit a blog post
    * Delete a blog post
    * Like or unlike a blog post
    * Comment, edit your comment or delete your comment on a blog post
    * Filter blog posts by topic
    * Administrator feature: add, edit and update topics for the blog

--- software_innovative_ideas_blog/blog/static/services/deletecomment.js
+++ software_innovative_ideas_blog/blog/static/services/deletecomment.js
@@ -19,3 +19,20 @@ function deleteComment(idea, ideaID, userID, commentID) {
 
     form.submit();
 }
+
+function goToAddComment(idea, ideaID, userID, commentID) {
+    document.getElementById('ideaID').value = ideaID
+    document.getElementById('userID').value = userID
+    document.getElementById('commentID').value = commentID
+    document.getElementById('authorID').value = userID 
+
+    var form = document.getElementById('addcomment');
+
+    form.appendChild(document.getElementById('ideaID'))
+    form.appendChild(document.getElementById('userID'))
+    form.appendChild(document.getElementById('commentID'))
+    form.appendChild(document.getElementById('authorID'))
+    form.appendChild(document.getElementById('comment'))
+
+    form.submit();
+}
--- software_innovative_ideas_blog/blog/static/services/editcomment.js
+++ software_innovative_ideas_blog/blog/static/services/editcomment.js
@@ -21,6 +21,24 @@ function editComment(idea, ideaID, userID, commentID) {
     form.submit();
 }
 
+function goToAddComment(idea, ideaID, userID, commentID) {
+    document.getElementById('ideaID').value = ideaID
+    document.getElementById('userID').value = userID
+    document.getElementById('commentID').value = commentID
+    document.getElementById('authorID').value = userID
+    document.getElementById('comment').value = document.getElementById('new_comment').value
+
+    var form = document.getElementById('addcomment');
+
+    form.appendChild(document.getElementById('ideaID'))
+    form.appendChild(document.getElementById('userID'))
+    form.appendChild(document.getElementById('commentID'))
+    form.appendChild(document.getElementById('authorID'))
+    form.appendChild(document.getElementById('comment'))
+
+    form.submit();
+}
+
 function deleteComment(ideaID, userID, comment) {
 
     document.getElementById('ideaID').value = ideaID
